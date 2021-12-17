b923d1f87ce332cf8db4091f3ac9bdc035acf0a1
mlanorero <mlanorero@gmail.com>
2021-12-17 02:06:40 +0000

    beginners python

--- exercises/13-Create-A-For-Loop/app.py
+++ exercises/13-Create-A-For-Loop/app.py
@@ -1,7 +1,7 @@
 def standards_maker():
     #your code here
     text = "I will write questions if I am stuck"
-    for i in range(1, 300):
+    for i in range(300):
         print(text)
 
 #remember to call the function outside (here)
